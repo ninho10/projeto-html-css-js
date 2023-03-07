5c91539ac665e2cc27c1844b69c7e08bf0596168
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-03-07 06:22:50 -0300

    Trabalho Final Font end
    Mudei o logo do site, coloquei o formulário do site conectado com meu e-mail toda mensagem vai direto para meu e-mail.

---
+++ Trabalho final Be-academi/script.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('nav ul');
+let menuBar = document.querySelector('nav .menu-icon');
+let iconMenu = document.querySelector('nav .menu-icon img');
+
+menuBar.addEventListener('click', function () {
+
+    if (iconMenu.getAttribute("src") == 'imagem/menu.png') {
+        iconMenu.setAttribute("src", "imagem/close.png");
+    } else {
+        iconMenu.setAttribute("src", "imagem/menu.png")
+    }
+
+    menu.classList.toggle('active')
+});
