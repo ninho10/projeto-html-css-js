8a2e4ac8e8cf17622acd70db486fcc4fdf7df165
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-04-11 22:10:48 -0300

    soma
    melhorando o código

--- Soma/soma.js
+++ Soma/soma.js
@@ -1,7 +1,7 @@
-let btn_soma = document.getElementById("btn_somarjs").addEventListener("click",()=>{
+let btn_somarjs = document.getElementById("btn_somarjs").addEventListener("click",()=>{
     let v1 = document.getElementById("v1").value
     let v2 = document.getElementById("v2").value
     soma = parseInt(v1) + parseInt(v2)
-    let res=document.getElementById("res").value=soma
+    let res = document.getElementById("res").value = soma
 })
 
